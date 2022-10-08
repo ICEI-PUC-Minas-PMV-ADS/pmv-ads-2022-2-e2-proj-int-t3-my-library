b1fa4e9a033629c26775a96d81b2892bc3acd22a
João Lessa <joaovictorlessa98@gmail.com>
2022-10-08 15:26:24 -0300

    Inicio do projeto

---
+++ src/Projeto/Projeto/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Projeto.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        //coleção de veiculos
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ src/Projeto/Projeto/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nome obrigatório!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Nome obrigatório!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Nome obrigatório!")]
+        public string CPF { get; set; }
+        [Required(ErrorMessage = "Nome obrigatório!")]
+        public string Telefone { get; set; }
+    }
+}
