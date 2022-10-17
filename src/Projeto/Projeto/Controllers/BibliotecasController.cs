aa2aaf164c3139e9c281614a7eef260f5254b0f6
João Lessa <joaovictorlessa98@gmail.com>
2022-10-17 13:38:57 -0300

    controllers e views das classes concluidos

---
+++ src/Projeto/Projeto/Controllers/BibliotecasController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Projeto.Models;
+
+namespace Projeto.Controllers
+{
+    public class BibliotecasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BibliotecasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bibliotecas
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Bibliotecas.Include(b => b.Usuario);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Bibliotecas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Bibliotecas == null)
+            {
+                return NotFound();
+            }
+
+            var biblioteca = await _context.Bibliotecas
+                .Include(b => b.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (biblioteca == null)
+            {
+                return NotFound();
+            }
+
+            return View(biblioteca);
+        }
+
+        // GET: Bibliotecas/Create
+        public IActionResult Create()
+        {
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "CPF");
+            return View();
+        }
+
+        // POST: Bibliotecas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UsuarioId,Nome,Compartilhar")] Biblioteca biblioteca)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(biblioteca);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "CPF", biblioteca.UsuarioId);
+            return View(biblioteca);
+        }
+
+        // GET: Bibliotecas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Bibliotecas == null)
+            {
+                return NotFound();
+            }
+
+            var biblioteca = await _context.Bibliotecas.FindAsync(id);
+            if (biblioteca == null)
+            {
+                return NotFound();
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "CPF", biblioteca.UsuarioId);
+            return View(biblioteca);
+        }
+
+        // POST: Bibliotecas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UsuarioId,Nome,Compartilhar")] Biblioteca biblioteca)
+        {
+            if (id != biblioteca.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(biblioteca);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BibliotecaExists(biblioteca.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "CPF", biblioteca.UsuarioId);
+            return View(biblioteca);
+        }
+
+        // GET: Bibliotecas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Bibliotecas == null)
+            {
+                return NotFound();
+            }
+
+            var biblioteca = await _context.Bibliotecas
+                .Include(b => b.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (biblioteca == null)
+            {
+                return NotFound();
+            }
+
+            return View(biblioteca);
+        }
+
+        // POST: Bibliotecas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Bibliotecas == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Bibliotecas'  is null.");
+            }
+            var biblioteca = await _context.Bibliotecas.FindAsync(id);
+            if (biblioteca != null)
+            {
+                _context.Bibliotecas.Remove(biblioteca);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BibliotecaExists(int id)
+        {
+          return _context.Bibliotecas.Any(e => e.Id == id);
+        }
+    }
+}
--- src/Projeto/Projeto/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/Projeto/Projeto/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,10 +17,131 @@ namespace Projeto.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.9")
+                .HasAnnotation("ProductVersion", "6.0.10")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
+
+            modelBuilder.Entity("Projeto.Models.Biblioteca", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+
+                    b.Property<bool>("Compartilhar")
+                        .HasColumnType("bit");
+
+                    b.Property<string>("Nome")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("UsuarioId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("UsuarioId");
+
+                    b.ToTable("Biblitoecas");
+                });
+
+            modelBuilder.Entity("Projeto.Models.Livro", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+
+                    b.Property<int>("Ano")
+                        .HasColumnType("int");
+
+                    b.Property<string>("Autor")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("BibliotecaId")
+                        .HasColumnType("int");
+
+                    b.Property<string>("Editora")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<bool>("Emprestar")
+                        .HasColumnType("bit");
+
+                    b.Property<string>("Genero")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("ISBN")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Local")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Nome")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("Paginas")
+                        .HasColumnType("int");
+
+                    b.Property<string>("Titulo")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("BibliotecaId");
+
+                    b.ToTable("Livros");
+                });
+
+            modelBuilder.Entity("Projeto.Models.Reserva", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+
+                    b.Property<int>("AvaliacaoConsulente")
+                        .HasColumnType("int");
+
+                    b.Property<int>("AvaliacaoProprietario")
+                        .HasColumnType("int");
+
+                    b.Property<DateTime>("DataDevolucaoAntecipada")
+                        .HasColumnType("datetime2");
+
+                    b.Property<DateTime>("DataFim")
+                        .HasColumnType("datetime2");
+
+                    b.Property<DateTime>("DataInicio")
+                        .HasColumnType("datetime2");
+
+                    b.Property<int>("LivroId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Status")
+                        .HasColumnType("int");
+
+                    b.Property<int>("UsuarioId")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("LivroId");
+
+                    b.HasIndex("UsuarioId");
+
+                    b.ToTable("Reservas");
+                });
 
             modelBuilder.Entity("Projeto.Models.Usuario", b =>
                 {
@@ -59,6 +180,47 @@ namespace Projeto.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Usuarios");
+                });
+
+            modelBuilder.Entity("Projeto.Models.Biblioteca", b =>
+                {
+                    b.HasOne("Projeto.Models.Usuario", "Usuario")
+                        .WithMany()
+                        .HasForeignKey("UsuarioId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Usuario");
+                });
+
+            modelBuilder.Entity("Projeto.Models.Livro", b =>
+                {
+                    b.HasOne("Projeto.Models.Biblioteca", "Biblioteca")
+                        .WithMany()
+                        .HasForeignKey("BibliotecaId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Biblioteca");
+                });
+
+            modelBuilder.Entity("Projeto.Models.Reserva", b =>
+                {
+                    b.HasOne("Projeto.Models.Livro", "Livro")
+                        .WithMany()
+                        .HasForeignKey("LivroId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.HasOne("Projeto.Models.Usuario", "Usuario")
+                        .WithMany()
+                        .HasForeignKey("UsuarioId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Livro");
+
+                    b.Navigation("Usuario");
                 });
 #pragma warning restore 612, 618
         }
