ca4d30c78e2b926e6f484eeaea9483bf7c05ec97
Carlos Marques <62437952+AsrothAbbadon@users.noreply.github.com>
2022-11-04 16:27:44 -0300

    sidemenu fix

--- src/Projeto/Projeto/wwwroot/js/site.js
+++ src/Projeto/Projeto/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function() {
+    let navs = document.querySelectorAll(".nav li")
+    navs = [...navs]
+    for (nav of navs) {
+        let link = nav.querySelectorAll("a")[0]
+        if (link.href.includes(location.pathname)) {
+            nav.classList.toggle("nav-item")
+        }
+    }
+})()
+
+//tggNav()
+
